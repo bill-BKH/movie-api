ef2ae329c6f46380236aca299643b149ba2e4131
bill-BKH <bill.bkh@gmail.com>
2025-10-29 14:25:39 +0330

    frontend project added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Card from "./components/Card";
+import PopularMovie from "./components/PopularMovie";
+
+
+function App() {
+  return (
+    <div className="container">
+      <PopularMovie></PopularMovie>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/PopularMovie.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card"
+
+
+function PopularMovie() {
+    return <div className="row">
+        <div className="col-4"><Card></Card></div>
+        <div className="col-4"><Card></Card></div>
+        <div className="col-4"><Card></Card></div>
+      </div>
+}
+
+export default PopularMovie;
